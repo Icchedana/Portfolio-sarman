65e63e74a701dd149497b10b0272fcc9e3f8072a
Md Sarman Rana <152283795+Icchedana@users.noreply.github.com>
2026-09-04 14:55:24 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        accent: {
+          DEFAULT: "#3b82f6",
+          hover: "#2563eb",
+        }
+      },
+    },
+  },
+  plugins: [],
+};
